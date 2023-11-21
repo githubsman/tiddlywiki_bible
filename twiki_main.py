0a0f76399651f478c059a2666cc69763aca8a4b7
Scott Morse <themanthursday@gmail.com>
2023-11-21 16:32:33 -0600

    consummate_tiddler
    break out function consummate_tiddler

--- twiki_main.py
+++ twiki_main.py
@@ -5,7 +5,7 @@ from regex_patterns import *
 from book_dictionary import *
 from regex_patterns import *
 from book_dictionary import *
-from config_params import *
+#from config_params import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s','--wiki_specimen', help='file path of source document', required=True, default='wiki_specimen/Wiki_Bible.html')
@@ -15,54 +15,68 @@ parser.add_argument('-cc','--count_curtail',help='', required=True, default='120
 args = parser.parse_args()
 wiki_specimen = args.wiki_specimen
 
+
 print(wiki_specimen)
 
+def consummate_tiddler():
+    print(tiddler_count, tid_type.upper(), tiddler_title)
+    print(tiddler['body'][:tiddler_truncate])
 
 
 
 output_mode     = 'debug'   #debug, debug_shorties, free text, wiki
-tiddler_count, count_curtail   = 0, 120
+tiddler_count, count_curtail, tiddler_truncate   = 0, 120, 100
 tid_body = ''
 tid_type = ''
+content = False
 wiki = []
 
 with open(wiki_specimen, encoding='utf8') as f:
 
     for line in f:
-        if re.match(rgx_comment_tiddler, line):
-            match = re.match(rgx_comment_tiddler, line)
-            if match:
-                if 'POST-SHADOW' not in tid_body and tid_body != '':
-                    tiddler = {'type': tid_type, 'book_abbr': match.group('bk'), 'chap': match.group('ch'), 'vs': match.group('vs'), 'body': tid_body }
-                    tiddler_title = f"{tiddler['book_abbr']}-{tiddler['chap']}-{tiddler['vs']}-note    {tiddler['body'][:60]}"
-                    print(tiddler_title)
-                    #print (tid_type, book_abbr, chap , vs, tid_body) 
-                    pass
+        if content == True:
+
+            if re.match(rgx_comment_tiddler, line):
+
+                consummate_tiddler(tiddler_count, tid_type, tiddler_title, tid_body)
+
+                match = re.match(rgx_comment_tiddler, line)
+
                 tid_type = 'commentry'
-                tid_body = ''
                 book_abbr = match.group('bk')
                 chap = match.group('ch')
                 vs = match.group('vs')
-                
-                #tiddler = {'type': 'comment', 'book_abbr': match.group('bk'), 'chap': match.group('ch'), 'vs': match.group('vs'), 'body': tid_body }
+                tiddler = {'type': tid_type, 'book_abbr': match.group('bk'), 'chap': match.group('ch'), 'vs': match.group('vs')}
+                tiddler_title = f"{tiddler['book_abbr']}-{tiddler['chap']}-{tiddler['vs']}"
 
-        elif re.match(rgx_scriptr_tiddler, line):
-            match = re.match(rgx_scriptr_tiddler, line)
-            if match:
-                if tid_body != '':
-                    tiddler = {'type': tid_type, 'book_abbr': match.group('bk'), 'chap': match.group('ch'), 'vs': match.group('vs'), 'body': tid_body }
-                    tiddler_title = f"{tiddler['book_abbr']}-{tiddler['chap']}-{tiddler['vs']}    {tiddler['body'][:60]}"
-                    print(tiddler_title)
-                    pass
+                tid_body = ''
+                tiddler_count +=1
+                
+                #tiddler = {'type': 'comment', 'book_abbr': match.group('bk'), 'chap': match.group('ch'), 'vs': match.group('vs')}
+
+            elif re.match(rgx_scriptr_tiddler, line):
+                
+                consummate_tiddler
+                
+                match = re.match(rgx_scriptr_tiddler, line)
+
                 tid_type = 'scripture'
                 tid_body = ''
+                tiddler = {'type': tid_type, 'book_abbr': match.group('bk'), 'chap': match.group('ch'), 'vs': match.group('vs'), 'body': tid_body }
+                tiddler_title = f"{tiddler['book_abbr']}-{tiddler['chap']}-{tiddler['vs']}"
+
                 book_abbr = match.group('bk')
                 chap = match.group('ch')
                 vs = match.group('vs')
+                tiddler_count +=1
 
-        else:
-            tid_body = f'{tid_body}\n{line}'
-
+            else:
+                #if tid_type != '':
+                tiddler['book_abbr'] = f'{tiddler["book_abbr"]}~{line.rstrip()}'
+                #pass
+        
+        if content == False:
+            content = 'storeArea' in line
 
 #   for line in f:
 #     if re.match(rgx_comment_tiddler, line):
