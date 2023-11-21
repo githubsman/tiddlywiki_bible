9ee65729a28f98fadd69872ddf1e3fceb7edd01e
Scott Morse <themanthursday@gmail.com>
2023-11-21 16:30:31 -0600

    Update regex_patterns.py

--- regex_patterns.py
+++ regex_patterns.py
@@ -1,21 +1,29 @@
 import re
 
-####### Patterns for TiddlyWiki doc
+####### Patterns to identify special-template tiddlers in my 
+#######     Bible TiddlyWiki document
 
-rgx_scriptr_tiddler = re.compile(r'''^<div\stitle=\"                    
-                                    (?P<bk>([1-3])?[A-Z][a-z]{1,20})\-  ## bk   = book_abbr
-                                    (?P<ch>\d{1,3})\-                   ## ch   = chap
-                                    (?P<vs>\d{1,3})\"                   ## vs   = vs
-                                    ''', re.VERBOSE)
+## MARK re.VERBOSE for legible comparison string.  
+##  Block comment allows easy read + explanations per line.
+##  VERBOSE says whitespace will be ignored (though :-[ it still shows in Code debug panel)
 
-rgx_comment_tiddler = re.compile(r'''^<div\stitle=\"
-                                    (?P<bk>([1-3])?[A-Z][a-z]{1,20})\-  ## bk   = book_abbr
-                                    (?P<ch>\d{1,3})\-                   ## ch   = chap
-                                    (?P<vs>\d{1,3})\-note\"             ## vs   = vs
-                                    ''', re.VERBOSE)
+rgx_scriptr_tiddler = re.compile(r'''
+    ^<div\stitle=\"                    
+    (?P<bk>([1-3])?[A-Z][a-z]{1,20})\-  ## bk   = book_abbr
+    (?P<ch>\d{1,3})\-                   ## ch   = chap
+    (?P<vs>\d{1,3})\"                   ## vs   = vs
+    ''', re.VERBOSE)
+
+## the COMMENTARY tiddler has the same syntax for title but with '-note' suffix. 
+rgx_comment_tiddler = re.compile(r'''
+    ^<div\stitle=\"
+    (?P<bk>([1-3])?[A-Z][a-z]{1,20})\-  ## bk   = book_abbr
+    (?P<ch>\d{1,3})\-                   ## ch   = chap
+    (?P<vs>\d{1,3})\-note\"             ## vs   = vs
+    ''', re.VERBOSE)
 
 
-
+## for dealing with markdown '----' (horiz line)
 #rgx_twiki_hz    = re.compile(r"\-\-\-\-")
 
 
